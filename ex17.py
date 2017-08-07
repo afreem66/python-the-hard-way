961947e2c9c5c924eaf1ae396b5755c2f3d28ab2
Andrew Freeman <afreeman@simplereach.com>
2017-08-07 17:25:43 -0400

    refactor with danny

--- ex17.py
+++ ex17.py
@@ -1,23 +1,33 @@
 from sys import argv
-from os.path import exists
+# from os.path import exists
 
 script, from_file, to_file = argv
 
-print(f"Copying from {from_file} to {to_file}")
+# print(f"Copying from {from_file} to {to_file}")
+#
+# in_file = open(from_file)
+# indata = in_file.read()
+#
+# print(f"The input file is {len(indata)} bytes long")
+#
+# print(f"Does the output file exist? {exists(to_file)}")
+# print("Ready, hit RETURN to contine, CTRL-C to abort.")
+# input()
+#
+# out_file = open(to_file, 'w')
+# out_file.write(indata)
+#
+# print("Alright, all done.")
+#
+# out_file.close()
+# in_file.close()
 
-in_file = open(from_file)
-indata = in_file.read()
+# show danny how to refactor
+# data_from_intial_file = open(from_file).read()
+#
+# print(f"The input file is {len(data_from_intial_file)} bytes long")
+#
+# open(to_file, 'w').write(data_from_intial_file)
 
-print(f"The input file is {len(indata)} bytes long")
-
-print(f"Does the output file exist? {exists(to_file)}")
-print("Ready, hit RETURN to contine, CTRL-C to abort.")
-input()
-
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
-print("Alright, all done.")
-
-out_file.close()
-in_file.close()
+# make it one line
+open(to_file, 'w').write(open(from_file).read())
