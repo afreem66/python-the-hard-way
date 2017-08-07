bbf4abbfbbb4ccae54ece5a5dcc1637d45dd30b1
Andrew Freeman <afreeman@simplereach.com>
2017-08-07 11:10:39 -0400

    first chapter

---
+++ ex5.py
@@ -0,0 +1,10 @@
+my_name = 'andrew freeman'
+my_age = 26
+my_height = 75
+my_weight = 200
+my_eyes = 'blue'
+my_hair = 'red'
+
+print(f"my name is {my_name}")
+print(f"I have {my_eyes} eyes and {my_hair} hair")
+print(f"I am {my_age} years old and {my_height} inches tall and weigh {my_weight}")
---
+++ ex7.py
@@ -0,0 +1,11 @@
+print("mary had a little lamb")
+print("it's fleece was white as {}".format("snow"))
+print('it went everywher that Mary went')
+print("," * 10)
+
+print("this is a line break \n")
+print("i didn't type a space")
+
+print("""this is a multiline string
+      I used three sets of quotes
+      check it out mannnnnnnnnnn""")
